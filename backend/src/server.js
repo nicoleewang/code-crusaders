7c056765d0bbbf2ac0a7c62f12b8df61035821d8
Lana Mireille Ong <z5590693@ad.unsw.edu.au>
2025-03-08 21:11:15 +1100

    committing swagger api documentation

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 import v1Routes from './routes/v1/server.js';
+import swaggerUi from 'swagger-ui-express'; 
+import YAML from 'yamljs';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,6 +14,9 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+// Swagger stuff
+const swaggerDocs = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/v1', v1Routes);
 
 const PORT = process.env.PORT || 5558;
